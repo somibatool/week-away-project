6901014aa5bef44ece8f69e8bfc05d292a915547
somi batool <somibatool44@gmail.com>
2024-04-26 11:55:36 +0500

    bug--fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,8 +10,8 @@ const NavBar = () => {
     setNav(!nav)
   }
   return (
-    <div className='w-full min-h-[50px] flex justify-between items-center absolute z-10 text-white bg-gray-700/80'>
-      <ul className='hidden sm:flex px-4'>
+    <div className='w-full min-h-[50px] overflow-y-hidden flex justify-between items-center absolute z-10 text-white bg-gray-700/80'>
+      <ul className='overflow-y-hidden sm:flex px-4'>
         <li>
           <a href="#">Home</a>
         </li>
@@ -37,7 +37,7 @@ const NavBar = () => {
         <FaBars size={20} className='mr-4 cursor-pointer'/>
       </div>
       {/* mobile menu */}
-      {/* <div onClick={handleNav} className={nav ? "overflow-y-hidden md:hidden lg:hidden ease-in duration-300 absolute text-gray-300 left-8 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col":'absolute top-0 h-screen left=[-100%] ease-in duration-500'}>
+      <div onClick={handleNav} className={nav ? "overflow-y-hidden  md:hidden  ease-in duration-300 absolute text-gray-300 left-8 top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col":'absolute top-0 h-screen left=[-100%] ease-in duration-500'}>
         <ul className='h-full w-full text-center pt-12'>
         <li className='text-2xl py-8'>
           <a href="#">Home</a>
@@ -53,26 +53,7 @@ const NavBar = () => {
         </li>
           
         </ul>
-      </div> */}
-      <div onClick={handleNav} className={`absolute top-0 w-full h-screen bg-black/90 px-4 py-7 flex flex-col ${nav ? "overflow-y-hidden md:hidden lg:hidden ease-in duration-300 text-gray-300 left-8" : "left-[100%] ease-in duration-500"}`}>
-    {nav && (
-        <ul className='h-full w-full text-center pt-12'>
-            <li className='text-2xl py-8'>
-                <a href="#">Home</a>
-            </li>
-            <li className='text-2xl py-8'>
-                <a href="#gallery">Gallery</a>
-            </li>
-            <li className='text-2xl py-8'>
-                <a href="#deals">Deals</a>
-            </li>
-            <li className='text-2xl py-8'>
-                <a href="#contact">Contact</a>
-            </li>
-        </ul>
-    )}
-</div>
-
+      </div>
     </div>
   );
 };
